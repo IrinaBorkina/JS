2ed38f6a20f33fc41a7663154a24248c6c1ecaec
Irina Borkina <irina_4ever@mail.ru>
2019-07-09 23:23:48 +0300

    feat: add rotation2

--- Tetris/src/game.js
+++ Tetris/src/game.js
@@ -30,33 +30,19 @@ export default class Game {
         get blocks() {
             return this.rotations[this.rotationIndex];
         },
-        rotationIndex: 0,
-        rotations: [
-            [
-                [0,1,0],
-                [1,1,1],
-                [0,0,0]
-            ],
-            [
-                [0,1,0],
-                [0,1,1],
-                [0,1,0]
-            ],
-            [
-                [0,0,0],
-                [1,1,1],
-                [0,1,0]
-            ],
-            [
-                [0,1,0],
-                [1,1,0],
-                [0,1,0]
-            ]
+        blocks: [
+            [0,1,0],
+            [1,1,1],
+            [0,0,0]
         ]
     };
 
     movePieceLeft() {
         this.activePiece.x -= 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
     }
 
     movePieceRight() {
@@ -77,13 +63,25 @@ export default class Game {
     }
 
     rotatePiece() {
-       this.activePiece.rotationIndex = this.activePiece.rotationIndex < 3 ? this.activePiece.rotationIndex + 1 : 0;
-       
-        if (this.hasCollision) {
-            this.activePiece.rotationIndex = this.activePiece.rotationIndex > 0 ? this.activePiece.rotationIndex - 1 : 3;
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
         }
 
-       return this.activePiece.blocks;
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+               temp[x][y] = blocks[length - 1 - y][x];
+            }
+        }
+
+        this.activePiece.blocks = temp;
+
+        if (this.hasCollision()) {
+            this.activePiece.blocks = blocks;
+        }
     }
 
     hasCollision() {
