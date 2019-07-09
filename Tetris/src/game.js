d97205c2f4cf3f7d594e2bdcdeb65a0a2d50b33a
Irina Borkina <irina_4ever@mail.ru>
2019-07-09 22:45:11 +0300

    feat: add move piece

--- Tetris/src/game.js
+++ Tetris/src/game.js
@@ -2,7 +2,7 @@ export default class Game {
     score = 0;
     lines = 0;
     level = 0;
-    playfied = [
+    playfield = [
         [0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0],
@@ -34,4 +34,54 @@ export default class Game {
         ]
     };
 
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+    }
+
+    movePieceRight() {
+        this.activePiece.x += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    }
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        }
+    }
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+        
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] && 
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+        
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
 }
