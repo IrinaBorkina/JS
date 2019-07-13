728ec822d26a278eadea2a872a18e90ec841a48e
Irina Borkina <irina_4ever@mail.ru>
2019-07-13 21:42:03 +0300

    feat: added movement of figures to the side

--- Tetris2/script.js
+++ Tetris2/script.js
@@ -136,4 +136,52 @@ function move() {
 
 let interval = setInterval(() => {
     move();
-}, 300);
+}, 400);
+
+let flag = true;
+
+window.addEventListener('keydown', function(e) {
+
+    let coordinates1 = [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')];
+    let coordinates2 = [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')];
+    let coordinates3 = [figureBody[2].getAttribute('posX'), figureBody[2].getAttribute('posY')];
+    let coordinates4 = [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')];
+
+    function getNewState(a) {
+
+        flag = true;
+
+        let figureNew = [
+            document.querySelector(`[posX = "${+coordinates1[0] + a}"][posY = "${coordinates1[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates2[0] + a}"][posY = "${coordinates2[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates3[0] + a}"][posY = "${coordinates3[1]}"]`),
+            document.querySelector(`[posX = "${+coordinates4[0] + a}"][posY = "${coordinates4[1]}"]`),
+        ];
+
+        for (let i = 0; i < figureNew.length; i++) {
+            if (!figureNew[i] || figureNew[i].classList.contains('set')) {
+                flag = false;
+            }
+        }
+
+        if (flag) {
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.remove('figure');
+            }
+
+            figureBody = figureNew;
+
+            for (let i = 0; i < figureBody.length; i++) {
+                figureBody[i].classList.add('figure');
+            }
+        }
+    }
+
+    if (e.keyCode == 37) {
+        getNewState(-1);
+    } else if (e.keyCode == 39) {
+        getNewState(1);
+    } else if (e.keyCode == 40) {
+        move();
+    }
+})
