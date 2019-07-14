4f9eb0880a8849c10b82de7f9c81b2c8aba0983f
Irina Borkina <irina_4ever@mail.ru>
2019-07-14 22:46:19 +0300

    feat: added game over

--- Tetris2/script.js
+++ Tetris2/script.js
@@ -1,6 +1,8 @@
+// поле для игры
 let tetris = document.createElement('div');
 tetris.classList.add('tetris');
 
+// заполняем поле ячейками
 for (let i = 1; i < 181; i++) {
     let excel = document.createElement('div');
     excel.classList.add('excel');
@@ -10,6 +12,7 @@ for (let i = 1; i < 181; i++) {
 let main = document.getElementsByClassName('main')[0];
 main.appendChild(tetris);
 
+// каждой ячейке присваиваем координаты
 let excel = document.getElementsByClassName('excel');
 let i = 0;
 
@@ -21,7 +24,10 @@ for (let y = 18; y > 0; y--) {
     }
 }
 
+// определяем стартовую позицию первой ячейки каждой фигуры
 let x = 5; y = 15;
+
+// главный многомерный массив, отрисовывающий все наши фигуры и во всех ротациях
 let mainArr = [
     //палка
     [
@@ -112,7 +118,7 @@ let mainArr = [
         [
             [1, -1],
             [1, -1],
-            [1, 0],
+            [-1, 0],
             [-1, 0]
         ],
         // поворот на 270 градусов
@@ -276,33 +282,39 @@ let mainArr = [
     ]
 ]
 
-let currentFigure = 0;
-let figureBody = 0;
-let rotate = 1;
+let currentFigure = 0;  // индекс текущей фигуры
+let figureBody = 0;  // фигура - 4 ячейки с классом 
+let rotate = 1; // переменная, которая будет отвечать за текущее состояние фигуры (повернута или она и на сколько градусов)
 
+// функция создания новой фигуры
 function create() {
     function getRandom() {
-        return Math.round(Math.random() * (mainArr.length - 1));
+        return Math.round(Math.random() * (mainArr.length - 1));  // получаем рандомное число от 0 до 6
     }
 
-    rotate = 1;
+    rotate = 1;  // возвращаем ротацию на исходную
     currentFigure = getRandom();
 
+    // используем это рандомное число - обращаемся к соответствующему элементу главного массива и достаем оттуда координаты
+    // первый элемент фигуры - это его нижняя правая ячейка и она всего будет иметь стартовые координаты [5,15]
     figureBody = [
         document.querySelector(`[posX = "${x}"][posY = "${y}"]`),
         document.querySelector(`[posX = "${x + mainArr[currentFigure][0][0]}"][posY = "${y + mainArr[currentFigure][0][1]}"]`),
         document.querySelector(`[posX = "${x + mainArr[currentFigure][1][0]}"][posY = "${y + mainArr[currentFigure][1][1]}"]`),
         document.querySelector(`[posX = "${x + mainArr[currentFigure][2][0]}"][posY = "${y + mainArr[currentFigure][2][1]}"]`)
-    ]
+    ];
 
     for (let i = 0; i < figureBody.length; i++) {
         figureBody[i].classList.add('figure');
     }
 }
-create();
+create(); // создаем первую фигуру
 
+// функция движения вниз
 function move() {
+    // флаг, в зависимости от состояния которого фигура либо падает дальше, либо останавливается 
     let moveFlag = true;
+    // текущие координаты всех четырех ячеек фигуры
     let coordinates = [
         [figureBody[0].getAttribute('posX'), figureBody[0].getAttribute('posY')],
         [figureBody[1].getAttribute('posX'), figureBody[1].getAttribute('posY')],
@@ -310,38 +322,79 @@ function move() {
         [figureBody[3].getAttribute('posX'), figureBody[3].getAttribute('posY')],
     ];
 
+    // проверяем, можно ли нам дальше падать стоит ли хоть одна ячейка фигуры на нижнем ряду поля и занят ли ряд под фигурой
+    // если хоть одна ячейка не проходит проверку, то наш флаг становится false
     for (let i = 0; i < coordinates.length; i++) {
         if (coordinates[i][1] == 1 || document.querySelector(`[posX = "${coordinates[i][0]}"][posY = "${coordinates[i][1] - 1}"]`).classList.contains('set')) {
             moveFlag = false;
             break;
         }
     }
-
+    // если движение вниз возможно, то делаем это
     if (moveFlag) {
+        // удаляем класс figure у фигуры
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.remove('figure');
         }
+        // перезаписываем координаты (понижаем ряд)
         figureBody = [
             document.querySelector(`[posX = "${coordinates[0][0]}"][posY = "${coordinates[0][1] - 1}"]`),
             document.querySelector(`[posX = "${coordinates[1][0]}"][posY = "${coordinates[1][1] - 1}"]`),
             document.querySelector(`[posX = "${coordinates[2][0]}"][posY = "${coordinates[2][1] - 1}"]`),
             document.querySelector(`[posX = "${coordinates[3][0]}"][posY = "${coordinates[3][1] - 1}"]`),
         ];
+         // ячейкам с новыми координатами задаем класс figure
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.add('figure');
         }
     } else {
+        // если фигура не может падать дальше, то фиксируем ее положение в пространстве, удаляем класс figure и ставим класс set
         for (let i = 0; i < figureBody.length; i++) {
             figureBody[i].classList.remove('figure');
             figureBody[i].classList.add('set');
+        }
+        // проверяем, появились ли заполненные ряды на поле
+        for (let i = 1; i < 15; i++) {
+            let count = 0;
+            for (let k = 1; k < 11; k++) {
+                if (document.querySelector(`[posX = "${k}"][posY = "${i}"]`).classList.contains('set')) {
+                    count++;
+                    if (count == 10) {
+                        for (let m = 1; m < 11; m++) {
+                            document.querySelector(`[posX = "${m}"][posY = "${i}"]`).classList.remove('set');
+                        }
+                        let set = document.querySelectorAll('.set');
+                        let newSet = [];
+                        for (let s = 0; s < set.length; s++) {
+                            let setCoordinates = [set[s].getAttribute('posX'), set[s].getAttribute('posY')];
+                            if (setCoordinates[1] > i) {
+                                set[s].classList.remove('set');
+                                newSet.push(document.querySelector(`[posX = "${setCoordinates[0]}"][posY = "${setCoordinates[1]-1}"]`));
+                            }
+                        }
+                        for (let a = 0; a < newSet.length; a++) {
+                            newSet[a].classList.add('set');
+                        }
+                        i--;
+                    }
+                }
+            }
+        }
+        for (let n = 1; n < 11; n++) {
+            if (document.querySelector(`[posX = "${n}"][posY = "15"]`).classList.contains('set')) {
+                clearInterval(interval);
+                alert('Игра окончена');
+                break;
+            }
         }
         create();
     }
 }
 
+//запускаем движение
 let interval = setInterval(() => {
     move();
-}, 400);
+}, 500);
 
 let flag = true;
 
@@ -386,7 +439,7 @@ window.addEventListener('keydown', function(e) {
         getNewState(-1);
     } else if (e.keyCode == 39) {
         getNewState(1);
-    } else if (e.keyCode == 40) {
+    } else if (e.keyCode == 40) {  //ускоряем функцию, нажимая на кнопку "вниз"
         move();
     } else if (e.keyCode == 38) {
         flag = true;
@@ -396,7 +449,7 @@ window.addEventListener('keydown', function(e) {
             document.querySelector(`[posX = "${+coordinates2[0] + mainArr[currentFigure][rotate + 2][1][0]}"][posY = "${+coordinates2[1] + mainArr[currentFigure][rotate + 2][1][1]}"]`),
             document.querySelector(`[posX = "${+coordinates3[0] + mainArr[currentFigure][rotate + 2][2][0]}"][posY = "${+coordinates3[1] + mainArr[currentFigure][rotate + 2][2][1]}"]`),
             document.querySelector(`[posX = "${+coordinates4[0] + mainArr[currentFigure][rotate + 2][3][0]}"][posY = "${+coordinates4[1] + mainArr[currentFigure][rotate + 2][3][1]}"]`),
-        ]
+        ];
 
         for (let i = 0; i < figureNew.length; i++) {
             if (!figureNew[i] || figureNew[i].classList.contains('set')) {
