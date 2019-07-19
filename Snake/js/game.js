943a5bc4d5df333e3cd4577e4e929421d5be3179
Irina Borkina <irina_4ever@mail.ru>
2019-07-20 00:33:39 +0300

    feat: add snake and food on field

--- Snake/js/game.js
+++ Snake/js/game.js
@@ -4,16 +4,32 @@ const ctx = canvas.getContext("2d");
 const ground = new Image();
 ground.src = "/img/ground.png";
 
-const food = new Image();
-food.src = "/img/food.png";
+const foodImg = new Image();
+foodImg.src = "/img/food.png";
 
 let box = 32;
 let score = 0;
 
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+};
 
+let snake = [];
+snake[0] = {
+    x: 9 * box,
+    y: 10 * box
+};
 
 function drawGame() {
     ctx.drawImage(ground, 0, 0);
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
 let game = setInterval(drawGame, 100);
