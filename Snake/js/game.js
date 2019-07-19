4bd3748a7d7bfaa1895ac404bd7ed3b49fc2ee7d
Irina Borkina <irina_4ever@mail.ru>
2019-07-20 00:20:16 +0300

    feat: add field for game

---
+++ Snake/js/game.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const ground = new Image();
+ground.src = "/img/ground.png";
+
+const food = new Image();
+food.src = "/img/food.png";
+
+let box = 32;
+let score = 0;
+
+
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+}
+
+let game = setInterval(drawGame, 100);
+
+
+
+
